fb4f4ac4f771c8d384ac9297ebaac4a96ab9b7e8
stdbreaks <sys32b@gmail.com>
2018-12-17 21:37:11 +0300

    Add files via upload

---
+++ custom_losses.py
@@ -0,0 +1,23 @@
+import keras.backend as K
+
+def dice_coef(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+def dice_loss(y_true, y_pred):
+    return 1. - dice_coef(y_true, y_pred)
+
+def jaccard_coef(y_true, y_pred):
+    # __author__ = Vladimir Iglovikov
+    intersection = K.sum(y_true * y_pred, axis=[0, -1, -2])
+    sum_ = K.sum(y_true + y_pred, axis=[0, -1, -2])
+
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+
+    return K.mean(jac)
+
+def jaccard_loss(y_true, y_pred):
+    return 1. - jaccard_coef(y_true, y_pred)
